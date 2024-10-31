14895211170fd2d09fe891b7976f96171e14c128
Lucas Åkerlund <lucas2001@hotmail.se>
2024-10-31 21:01:42 +0100

    Add material icons support

--- frontend/src/pages/tasks/tasks.tsx
+++ frontend/src/pages/tasks/tasks.tsx
@@ -1,17 +1,18 @@
-import { Task } from "../../models/task";
+import {Task} from "../../models/task";
 import "./tasks.scss"
 
-export const Tasks = ({title, tasks}: {title: string, tasks: Task[]}) => {
+export const Tasks = ({title, tasks}: { title: string, tasks: Task[] }) => {
     return (
         <div className="tasks-page">
             <h2>{title}</h2>
-            
+
             {tasks.map(task => {
                 return (
-                   <div className="task-container">
+                    <div className="task-container">
                         <div className="title">{task.title}</div>
                         <div className="description">{task.description}</div>
-                   </div>
+                        <span className="icon icon-edit"></span>
+                    </div>
                 )
             })}
         </div>
